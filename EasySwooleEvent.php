82e37cf87955d1f85af4ca0ae1114d06cf3219cc
Allen <1107806823@qq.com>
2019-02-28 19:44:47 +0800

    easyswoole event

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -13,6 +13,7 @@ use EasySwoole\EasySwoole\Swoole\EventRegister;
 use EasySwoole\EasySwoole\AbstractInterface\Event;
 use EasySwoole\Http\Request;
 use EasySwoole\Http\Response;
+use EasySwoole\Utility\File;
 
 class EasySwooleEvent implements Event
 {
@@ -21,6 +22,7 @@ class EasySwooleEvent implements Event
     {
         // TODO: Implement initialize() method.
         date_default_timezone_set('Asia/Shanghai');
+        self::loadConf();
     }
 
     public static function mainServerCreate(EventRegister $register)
@@ -37,5 +39,23 @@ class EasySwooleEvent implements Event
     public static function afterRequest(Request $request, Response $response): void
     {
         // TODO: Implement afterAction() method.
+    }
+
+    /**
+     * 引用自定义配置文件
+     * @throws \Exception
+     */
+    public static function loadConf()
+    {
+        $files = File::scanDirectory(EASYSWOOLE_ROOT . '/App/Config');
+        if (is_array($files)) {
+            foreach ($files['files'] as $file) {
+                $fileNameArr = explode('.', $file);
+                $fileSuffix = end($fileNameArr);
+                if ($fileSuffix == 'php') {
+                    Config::getInstance()->loadFile($file);
+                }
+            }
+        }
     }
 }
