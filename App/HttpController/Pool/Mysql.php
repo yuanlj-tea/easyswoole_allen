cb6708317aefc2e18190a1ef2f79839697c7acf1
Allen <1107806823@qq.com>
2019-03-18 09:37:37 +0800

    测试生成唯一uid

--- App/HttpController/Pool/Mysql.php
+++ App/HttpController/Pool/Mysql.php
@@ -13,14 +13,47 @@ use App\HttpController\Base\BaseMysql;
 use App\Model\User\UserBean;
 use App\Model\User\UserModel;
 use EasySwoole\Http\Message\Status;
+use EasySwoole\Utility\SnowFlake;
 
 class Mysql extends BaseMysql
 {
     public function index()
     {
         $db = $this->getDbConnection();
-        $data = $db->where('id',1,'=')->get('test');
-        $this->writeJson(1,$data);
+        $str = $this->getUid();
+        $db->insert('t',['str'=>$str]);
+
+        // $str = SnowFlake::make(20,1);
+        // $str = gen_uid();
+        // $db->insert('t',['str'=>$str]);
+
+        // $data = $db->where('id',1,'=')->get('test');
+        // $this->writeJson(1,$data);
+    }
+
+    public function getUid()
+    {
+        while(true){
+            //订购日期
+            $order_date = date('Y-m-d');
+            //订单号码主体（YYYYMMDDHHIISSNNNNNNNN）
+            $order_id_main = date('YmdHis') . rand(10000000,99999999);
+            //订单号码主体长度
+            $order_id_len = strlen($order_id_main);
+            $order_id_sum = 0;
+            for($i=0; $i<$order_id_len; $i++){
+                $order_id_sum += (int)(substr($order_id_main,$i,1));
+            }
+            //唯一订单号码（YYYYMMDDHHIISSNNNNNNNNCC）
+            $order_id = $order_id_main . str_pad((100 - $order_id_sum % 100) % 100,2,'0',STR_PAD_LEFT);
+
+            $db = $this->getDbConnection();
+            $data = $db->where('str',$order_id,'=')->getOne('t','*');
+            if(empty($data)){
+                return $order_id;
+                break;
+            }
+        }
     }
 
     public function getAll()
